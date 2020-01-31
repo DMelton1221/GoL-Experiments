2079f1e48976f073560fb8a98b7f8c261bf8c6d8
D Melton <55304913+DMelton1221@users.noreply.github.com>
2020-01-31 15:46:40 -0800

    Render a heightmap
    Renders a heightmap that is generated using the steps from each board state from Life, also changed some variable names to be clearer.

--- Assets/GoLHeightMap.cs
+++ Assets/GoLHeightMap.cs
@@ -5,36 +5,60 @@ using UnityEngine;
 public class GoLHeightMap : MonoBehaviour
 {
 
-    private Texture2D tex;
-    private GridType<int> HeightMap;
+    private Texture2D texBoard;
+    private Texture2D texMap;
+    private GridType<int> heightMap;
     private GoL_logic game;
 
     GoLHeightMap() {
         game = new GoL_logic();
+        heightMap = new GridType<int>(game.GetBoard().Width(), game.GetBoard().Height());
     }
 
 
     public void RenderBoard() {
-        var currentColor = this.tex.GetRawTextureData<Color32>();
+        var currentColor = texBoard.GetRawTextureData<Color32>();
+        var game_board = game.GetBoard();
+        int w = game_board.Width();
+        int h = game_board.Height();
+        var colW = new Color32(255,255,255,255);
+        var colB = new Color32(0,0,0,255);
 
-        for(int i = 0; i < tex.width * tex.height; i++ ) {
-            var game_board = game.GetBoard();
-            int w = game_board.Width();
-            int h = game_board.Height();
-
-            var colW = new Color32(255,255,255,255);
-            var colB = new Color32(0,0,0,255);
-
+        for(int i = 0; i < texBoard.width * texBoard.height; i++ ) {
             currentColor[i] = game_board[i % w, i / h] ? colW : colB;
-
         }
-        tex.Apply();
+        texBoard.Apply();
+    }
+
+    public void RenderHeightMap() {
+        var currentColor = texMap.GetRawTextureData<Color32>();
+        int w = heightMap.Width();
+        int h = heightMap.Height();
+
+        for(int i = 0; i < texBoard.width * texBoard.height; i++ ) {
+            byte component = (byte) heightMap[i % w, i / h];
+
+            currentColor[i] = new Color32(component, component, component, 255);
+        }
+        texMap.Apply();
+    }
+
+    public void SumBoards() {
+        var h = heightMap.Height();
+        var w = heightMap.Width();
+        for(int j = 0; j < h; j++ ) {
+            for(int i = 0; i < w;  i++) {
+                heightMap[i, j] += game.GetBoard()[i, j] ? 1 : 0;
+            }
+        }
     }
 
     void Awake() {
         game.Populate();
-        tex = new Texture2D(game.GetBoard().Width(), game.GetBoard().Height(), TextureFormat.RGBA32, false);
-        tex.filterMode = FilterMode.Point;
+        texBoard = new Texture2D(game.GetBoard().Width(), game.GetBoard().Height(), TextureFormat.RGBA32, false);
+        texBoard.filterMode = FilterMode.Point;
+
+        texMap = new Texture2D(heightMap.Width(), heightMap.Height(), TextureFormat.RGBA32, false);
 
     }
     // Start is called before the first frame update
@@ -46,9 +70,12 @@ public class GoLHeightMap : MonoBehaviour
     void Update() {
         game.NextBoardState();
         RenderBoard();
+        SumBoards();
+        RenderHeightMap();
     }
 
     void OnRenderObject() {
-        Graphics.DrawTexture(new Rect(0, 0, 1, 1), tex);
+        Graphics.DrawTexture(new Rect(0, 0, 1, 1), texBoard);
+        Graphics.DrawTexture(new Rect(1, 0, 1, 1), texMap);
     }
 }
